f4daf4d0662393ebd56b8ef07c8e4d63d3e02ff8
DemonicDiviner <160653530+DemonicDiviner@users.noreply.github.com>
2025-02-13 16:39:28 +0300

    q

---
+++ CONFIG.js
@@ -0,0 +1,10 @@
+const CONFIGDATA = {
+  titleHeader: "Привет, го встр! </br> Если че это шутка, но может и нет :) </br> Ведь в каждой шутке есть доля правды",
+  descriptionHeader:
+    "",
+  buttonYes: "Я согласна",
+  buttonNo: "Не согласна",
+  titleModar: "Спиздил красивые цитаты.",
+  descriptionModar:
+    "Ты — весь мой мир. </br> Люблю тебя <3",
+};
